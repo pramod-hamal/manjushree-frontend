e8a6ef4b6d68ee2b5bf37b85dacf15177241a571
LeanQChris <chris.leanq@gmail.com>
2023-08-13 12:35:21 +0545

    Hydration Error on Header fixed

--- src/components/layouts/dashboard/DashboardLayout.tsx
+++ src/components/layouts/dashboard/DashboardLayout.tsx
@@ -4,18 +4,25 @@ import React, { memo, useState } from "react";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
-function DashboardLayoutComponent({ children, title }: { title?: string, children: React.ReactNode }) {
+function DashboardLayoutComponent({
+  children,
+  title,
+}: {
+  title?: string;
+  children: React.ReactNode;
+}) {
   const [minimize, setMinize] = useState<boolean>(false);
   const handleToogle = () => setMinize(!minimize);
   return (
     <div className="flex transition-all">
       <Sidebar minimize={minimize} handleToogle={handleToogle} />
       <main
-        className={`transition-all flex flex-col ${minimize ? "pl-[55px] w-[97%]" : "pl-[13%] w-[87%]"
-          } `}
+        className={`transition-all flex flex-col ${
+          minimize ? "pl-[55px] w-[97%]" : "pl-[13%] w-[87%]"
+        } `}
       >
         <Header />
-        <div className="p-5">{children}</div>
+        <section className="p-5">{children}</section>
       </main>
     </div>
   );

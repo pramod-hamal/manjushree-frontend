38468d45fe0536bfa8a96e666a4fec1ca9c20066
LeanQ Digital <leanqdigital@LeanQs-Mac-mini.local>
2023-09-19 11:51:51 +0545

    feature: Added individual contact form on both participant and user contact form , which add individual contact and then refetch the dropdown for the new contact

--- src/app/(protected)/participants/[id]/components/contact/ContactForm.tsx
+++ src/app/(protected)/participants/[id]/components/contact/ContactForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFormik } from "formik";
 import { PlusOutlined } from "@ant-design/icons";
 
@@ -15,11 +15,13 @@ import {
 } from "@/store/hooksleanq_support_coordinator";
 import { participantDetailState } from "@/store/features/participants/detail/participantDetailSliceleanq_support_coordinator";
 import { toogleModal } from "@/store/features/participants/contact/contactDetailSliceleanq_support_coordinator";
-import { useOrganizationContactQuery } from "@/store/features/dropdown/apiSliceleanq_support_coordinator";
+import { dropdownApi, useContactQuery } from "@/store/features/dropdown/apiSliceleanq_support_coordinator";
 
 import { useToast } from "@/core/lib/toast/useToastleanq_support_coordinator";
 import FormInput from "@/components/form/FormInputleanq_support_coordinator";
 import ErrorMessage from "@/components/form/ErrorMessageleanq_support_coordinator";
+import CusModal from "@/components/modals/Modalleanq_support_coordinator";
+import IndividualContactForm from "@/app/(protected)/contact/individual/add/components/IndividualContactFormleanq_support_coordinator";
 
 export interface ParticipantContactDTO {
   relation: string;
@@ -28,13 +30,17 @@ export interface ParticipantContactDTO {
 }
 
 export default function ContactForm() {
+  const [showContactForm, setShowContactForm] = useState<boolean>(false);
+  
   const [addContact] = useAddMutation();
   const { participantDetail } = useAppSelector(participantDetailState);
-  const { data}: any = useOrganizationContactQuery("");
+  const { data ,refetch}: any = useContactQuery("");
 
   const dispatch = useAppDispatch();
 
   const showToast = useToast();
+
+  const handleToogleShow = () => setShowContactForm(!showContactForm)
 
   const initialValues: ParticipantContactDTO = {
     relation: "",
@@ -51,7 +57,6 @@ export default function ContactForm() {
       showToast({ title: "Contact Added", type: "success" });
       formik.resetForm()
     }).catch((error: any) => {
-      console.log(error)
       formik.setErrors(error?.data?.error);
       showToast({ title: error?.data?.message, type: "error" });
     }).finally(() => {
@@ -69,49 +74,57 @@ export default function ContactForm() {
   });
 
   return (
-    <form className="p-5 gap-5 flex flex-col" onSubmit={formik.handleSubmit}>
-      <h3 className="text-2xl font-semibold m-0">Add Contact</h3>
-      <div className="flex flex-col gap-5">
-        <div className="flex gap-5">
-          <div className="w-[366px]">
-            <CusSelect
-              placeHolder="Select Contact"
-              onChange={(selectedData: any) => {
-                formik.setFieldValue("contactId", selectedData);
-              }}
-              options={data?.data ?? []}
-              value={formik.values.contactId}
-            />
-            {formik.errors.contactId && <ErrorMessage message={formik.errors.contactId}/>}
+    <>
+      <form className="p-5 gap-5 flex flex-col" onSubmit={formik.handleSubmit}>
+        <h3 className="text-2xl font-semibold m-0">Add Contact</h3>
+        <div className="flex flex-col gap-5">
+          <div className="flex gap-5">
+            <div className="w-[366px]">
+              <CusSelect
+                placeHolder="Select Contact"
+                onChange={(selectedData: any) => {
+                  formik.setFieldValue("contactId", selectedData);
+                }}
+                options={data?.data ?? []}
+                value={formik.values.contactId}
+              />
+              {formik.errors.contactId && <ErrorMessage message={formik.errors.contactId} />}
+            </div>
+            <div>
+              <FlatButton
+                icon={<PlusOutlined />}
+                title="Create New"
+                type="button"
+                onClick={handleToogleShow}
+                color="text-black bg-white border border-solid text-xs shadow  border-[#1890FF] text-primary-title"
+              />
+            </div>
           </div>
-         <div>
-         <FlatButton
-            icon={<PlusOutlined />}
-            title="Create New"
-            type="button"
-            onClick={() => { }}
-            color="text-black bg-white border border-solid text-xs shadow  border-[#1890FF] text-primary-title"
-          />
-         </div>
+          <div className="w-[366px]">
+            <FormInput
+              placeHolder="Relationship"
+              onChange={formik.handleChange}
+              name="relation"
+              errors={formik.errors.relation}
+              value={formik.values.relation}
+            />
+          </div>
         </div>
-        <div className="w-[366px]">
-          <FormInput
-            placeHolder="Relationship"
-            onChange={formik.handleChange}
-            name="relation"
-            errors={formik.errors.relation}
-            value={formik.values.relation}
+        <div className="flex gap-10 items-center">
+          <FlatButton
+            title="Submit"
+            type="submit"
+            loading={formik.isSubmitting}
           />
+          <CancelButton />
         </div>
-      </div>
-      <div className="flex gap-10 items-center">
-        <FlatButton
-          title="Submit"
-          type="submit"
-          loading={formik.isSubmitting}
-        />
-        <CancelButton />
-      </div>
-    </form>
+      </form>
+      <CusModal width={800} show={showContactForm} onClose={handleToogleShow}>
+        <IndividualContactForm handleOnClose={() => {
+          refetch();
+          handleToogleShow();
+        }} />
+      </CusModal>
+    </>
   );
 }
--- src/app/(protected)/users/[id]/components/contact/ContactForm.tsx
+++ src/app/(protected)/users/[id]/components/contact/ContactForm.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useFormik } from "formik";
 import { PlusOutlined } from "@ant-design/icons";
 
@@ -9,7 +9,7 @@ import CusSelect from "@/components/form/Selectleanq_support_coordinator";
 
 import { APIBaseResponse } from "@/core/interface/api.responseleanq_support_coordinator";
 import { useAppSelector } from "@/store/hooksleanq_support_coordinator";
-import { useOrganizationContactQuery } from "@/store/features/dropdown/apiSliceleanq_support_coordinator";
+import { useContactQuery } from "@/store/features/dropdown/apiSliceleanq_support_coordinator";
 
 import { useToast } from "@/core/lib/toast/useToastleanq_support_coordinator";
 import {
@@ -20,13 +20,19 @@ import { UserContactDTO } from "@/store/features/users/interface/user.contact.in
 import { useAddContactMutation } from "@/store/features/users/apiSliceleanq_support_coordinator";
 import FormInput from "@/components/form/FormInputleanq_support_coordinator";
 import ErrorMessage from "@/components/form/ErrorMessageleanq_support_coordinator";
+import CusModal from "@/components/modals/Modalleanq_support_coordinator";
+import IndividualContactForm from "@/app/(protected)/contact/individual/add/components/IndividualContactFormleanq_support_coordinator";
 
 export default function ContactForm({ onClose }: { onClose: () => void }) {
+  const [showContactForm, setShowContactForm] = useState<boolean>(false);
+
   const { userDetail }: UserSliceState = useAppSelector(userState);
-  const { data }: any = useOrganizationContactQuery("");
+  const { data, refetch }: any = useContactQuery("");
   const [addContact] = useAddContactMutation();
 
   const showToast = useToast();
+
+  const handleToogleShow = () => setShowContactForm(!showContactForm)
 
   const initialValues: UserContactDTO = {
     relation: "",
@@ -57,49 +63,57 @@ export default function ContactForm({ onClose }: { onClose: () => void }) {
   });
 
   return (
-    <form className="p-5 gap-5 flex flex-col" onSubmit={formik.handleSubmit}>
-      <h3 className="text-2xl font-semibold m-0">Add Contact</h3>
-      <div className="flex flex-col gap-5">
-        <div className="flex gap-5">
+    <>
+      <form className="p-5 gap-5 flex flex-col" onSubmit={formik.handleSubmit}>
+        <h3 className="text-2xl font-semibold m-0">Add Contact</h3>
+        <div className="flex flex-col gap-5">
+          <div className="flex gap-5">
+            <div className="w-[366px]">
+              <CusSelect
+                placeHolder="Select Contact"
+                onChange={(selectedData: any) => {
+                  formik.setFieldValue("contactId", selectedData);
+                }}
+                options={data?.data ?? []}
+                value={formik.values.contactId}
+              />
+              {formik.errors.contactId && <ErrorMessage message={formik.errors.contactId} />}
+            </div>
+            <div>
+              <FlatButton
+                icon={<PlusOutlined />}
+                title="Create New"
+                type="button"
+                onClick={handleToogleShow}
+                color="text-black bg-white border border-solid text-xs shadow  border-[#1890FF] text-primary-title"
+              />
+            </div>
+          </div>
           <div className="w-[366px]">
-            <CusSelect
-              placeHolder="Select Contact"
-              onChange={(selectedData: any) => {
-                formik.setFieldValue("contactId", selectedData);
-              }}
-              options={data?.data ?? []}
-              value={formik.values.contactId}
-            />
-            {formik.errors.contactId && <ErrorMessage message={formik.errors.contactId} />}
-          </div>
-          <div>
-            <FlatButton
-              icon={<PlusOutlined />}
-              title="Create New"
-              type="button"
-              onClick={() => { }}
-              color="text-black bg-white border border-solid text-xs shadow  border-[#1890FF] text-primary-title"
+            <FormInput
+              placeHolder="Relationship"
+              onChange={formik.handleChange}
+              name="relation"
+              errors={formik.errors?.relation}
+              value={formik.values.relation}
             />
           </div>
         </div>
-        <div className="w-[366px]">
-          <FormInput
-            placeHolder="Relationship"
-            onChange={formik.handleChange}
-            name="relation"
-            errors={formik.errors?.relation}
-            value={formik.values.relation}
+        <div className="flex gap-10 items-center">
+          <FlatButton
+            title="Submit"
+            type="submit"
+            loading={formik.isSubmitting}
           />
+          <CancelButton />
         </div>
-      </div>
-      <div className="flex gap-10 items-center">
-        <FlatButton
-          title="Submit"
-          type="submit"
-          loading={formik.isSubmitting}
-        />
-        <CancelButton />
-      </div>
-    </form>
+      </form>
+      <CusModal width={800} show={showContactForm} onClose={handleToogleShow}>
+        <IndividualContactForm handleOnClose={() => {
+          refetch();
+          handleToogleShow();
+        }} />
+      </CusModal>
+    </>
   );
 }

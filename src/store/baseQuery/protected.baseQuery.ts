12ca6cfae4737ede66c4ac40466f6470b78b14a7
LeanQ Digital <leanqdigital@LeanQs-Mac-mini.local>
2023-09-19 12:33:31 +0545

    feature: Handle unauthorized api calls and redirected to login page

--- src/store/baseQuery/protected.baseQuery.ts
+++ src/store/baseQuery/protected.baseQuery.ts
@@ -3,7 +3,7 @@ import { prepareAuthHeader } from "@/core/lib/getHeadersleanq_support_coordinato
 
 import { BaseQueryFn, FetchArgs, FetchBaseQueryError, fetchBaseQuery } from "@reduxjs/toolkit/dist/query";
 
- export const apiQuery = fetchBaseQuery({
+export const apiQuery = fetchBaseQuery({
   baseUrl: baseUrl,
   prepareHeaders: prepareAuthHeader,
 });
@@ -16,6 +16,7 @@ export const protectedBaseQuery: BaseQueryFn<
   let result = await apiQuery(args, api, extraOptions)
   if (result.error && result.error.status === 401) {
     localStorage.clear();
+    location.reload()
   }
   return result
 }

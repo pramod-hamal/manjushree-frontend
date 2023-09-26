452e776a5b8e46bcaf2b160d943fc9c6bac92e15
LeanQChris <chris.leanq@gmail.com>
2023-09-26 11:43:55 +0545

    feature: removed edit button from participant plan, added copy icons, added feature to copy the remaining days by clicking the copy icon

--- src/app/(protected)/participants/[id]/components/plan/Summary.tsx
+++ src/app/(protected)/participants/[id]/components/plan/Summary.tsx
@@ -1,16 +1,20 @@
 import React, { useState } from "react";
 import { Skeleton } from "antd";
-import { EyeOutlined, PlusOutlined } from "@ant-design/icons";
+import { CopyOutlined, PlusOutlined } from "@ant-design/icons";
 
 import {
   daysDifference,
   defaultDateFormat,
 } from "@/core/lib/date.utilsleanq_support_coordinator";
+
 import FlatButton from "@/components/buttons/Buttonleanq_support_coordinator";
 import CusModal from "@/components/modals/Modalleanq_support_coordinator";
+
 import CreatePlan from "./CreatePlanForm";
+
 import useGetParticipantPlan, { GetParticipantPlanProps } from "../../hook/useGetParticipantPlan";
 import useGetParticipantDetail from "../../hook/useGetParticipant";
+import { copyTextToClipboard } from "@/core/lib/copyToClipboardleanq_support_coordinator";
 
 export default function Summary() {
   const [showModal, setShowModal] = useState<boolean>(false);
@@ -36,7 +40,7 @@ export default function Summary() {
             <div className="flex items-center gap-5 justify-between">
               <span className="font-semibold text-sm">{defaultDateFormat(plan.startDate)} -{defaultDateFormat(plan.endDate)}</span>
               <span>
-                <EyeOutlined className="text-primary-title mr-5" />
+                <CopyOutlined className="text-primary-title mr-5" />
               </span>
             </div>
             <span className="text-xs text-gray-400">Days Remaining</span>
@@ -44,7 +48,7 @@ export default function Summary() {
               <span className="font-semibold text-sm">
                 {daysDifference(defaultDateFormat(plan.startDate), defaultDateFormat(plan.endDate))}
               </span>
-              <span> <EyeOutlined className="text-primary-title mr-5" /></span>
+              <span> <CopyOutlined className="text-primary-title mr-5" onClick={() => { copyTextToClipboard(daysDifference(defaultDateFormat(plan.startDate), defaultDateFormat(plan.endDate)).toString()) }} /></span>
             </div>
           </>
         ) : (
@@ -63,3 +67,4 @@ export default function Summary() {
     </div>
   );
 }
+

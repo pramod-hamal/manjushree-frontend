67b958a4154a3d94468e3f712be16d5c48598f59
LeanQChris <chris.leanq@gmail.com>
2023-10-12 11:35:05 +0545

    fix:plan document import error

--- src/app/support-coordinator/participants/[id]/components/plan/document/hook/usePlanDocumentList.tsx
+++ src/app/support-coordinator/participants/[id]/components/plan/document/hook/usePlanDocumentList.tsx
@@ -1,6 +1,5 @@
 import { participantDetailState } from "@/store/features/participants/detail/participantDetailSliceleanq_support_coordinator";
-import { useGetAllDocumentsQuery } from "@/store/features/participants/documents/apiSliceleanq_support_coordinator";
-import { useParticipantPlanQuery } from "@/store/features/participants/plan/apiSliceleanq_support_coordinator";
+import { useParticipantPlanQuery, useGetAllDocumentsQuery } from "@/store/features/participants/plan/apiSliceleanq_support_coordinator";
 import { PlanInterface, PlanResponse } from "@/store/features/participants/plan/interface/plan.interfaceleanq_support_coordinator";
 import { useAppSelector } from "@/store/hooksleanq_support_coordinator";
 
@@ -12,7 +11,9 @@ const usePlanDocumentList = () => {
     const plan: PlanInterface | null =
         planData && planData.length > 0 ? planData[0] : null;
 
-    const { data, isLoading, isFetching, error } = useGetAllDocumentsQuery(plan?.id!);
+    const { data, isLoading, isFetching, error } = useGetAllDocumentsQuery({
+        plan: plan?.id!,
+    });
 
     return { data, isLoading, isFetching, error }
 }
